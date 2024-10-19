de6e9707a8360375b5009e6cefd7f6060524ae18
Daojie-u20 <Daojie.Peng@outlook.com>
2024-10-19 17:46:41 +0800

    initialized 'lm_xxx_go2' task;  managed 'command', 'obs' with language terms; -v1 can train but slow;

--- source/extensions/omni.isaac.lab/omni/isaac/lab/envs/mdp/commands/velocity_command.py
+++ source/extensions/omni.isaac.lab/omni/isaac/lab/envs/mdp/commands/velocity_command.py
@@ -20,6 +20,12 @@ if TYPE_CHECKING:
     from omni.isaac.lab.envs import ManagerBasedEnv
 
     from .commands_cfg import NormalVelocityCommandCfg, UniformVelocityCommandCfg
+    from .commands_cfg import LMVelocityCommandCfg # pdj: configuration class for language-motion velocity commands 
+
+# pdj: import language related packages
+from transformers import AutoTokenizer, AutoModel 
+# from transformers import DPRContextEncoder, DPRContextEncoderTokenizer
+
 
 
 class UniformVelocityCommand(CommandTerm):
@@ -197,6 +203,212 @@ class UniformVelocityCommand(CommandTerm):
 
         return arrow_scale, arrow_quat
 
+
+class LMVelocityCommand(CommandTerm):
+    r"""Command generator that generates a velocity command in SE(2) from uniform distribution.
+
+    The command comprises of a linear velocity in x and y direction and an angular velocity around
+    the z-axis. It is given in the robot's base frame.
+
+    If the :attr:`cfg.heading_command` flag is set to True, the angular velocity is computed from the heading
+    error similar to doing a proportional control on the heading error. The target heading is sampled uniformly
+    from the provided range. Otherwise, the angular velocity is sampled uniformly from the provided range.
+
+    Mathematically, the angular velocity is computed as follows from the heading command:
+
+    .. math::
+
+        \omega_z = \frac{1}{2} \text{wrap_to_pi}(\theta_{\text{target}} - \theta_{\text{current}})
+
+    """
+
+    cfg: LMVelocityCommandCfg
+    """The configuration of the command generator."""
+
+    def __init__(self, cfg: LMVelocityCommandCfg, env: ManagerBasedEnv):
+        """Initialize the command generator.
+
+        Args:
+            cfg: The configuration of the command generator.
+            env: The environment.
+        """
+        # initialize the base class
+        super().__init__(cfg, env)
+
+        # obtain the robot asset
+        # -- robot
+        self.robot: Articulation = env.scene[cfg.asset_name]
+
+        # crete buffers to store the command
+        # -- command: x vel, y vel, yaw vel, heading
+        self.vel_command_b = torch.zeros(self.num_envs, 3, device=self.device)
+
+        # pdj: using dragon-multiturn context encoder to encode the velocity commands
+        # self.tokenizer = DPRContextEncoderTokenizer.from_pretrained('facebook/dpr-ctx_encoder-single-nq-base')
+        # self.context_encoder = DPRContextEncoder.from_pretrained('facebook/dpr-ctx_encoder-single-nq-base')
+        self.tokenizer = AutoTokenizer.from_pretrained('nvidia/dragon-multiturn-query-encoder')
+        self.context_encoder = AutoModel.from_pretrained('nvidia/dragon-multiturn-context-encoder')
+        self.init_express = "I am running at longitudinal speed: {} m/s, lateral speed: {} m/s, spinning speed: {} m/s.".format(0.0, 0.0, 0.0)
+        self.init_tokens = self.tokenizer(self.init_express, padding=True, truncation=True, max_length=512, return_tensors='pt')
+        self.init_context = self.context_encoder(**self.init_tokens).pooler_output # (1, emb_dim=768)
+        # I want to embed all 3 vel into one embedding, so the shape should be [num_envs, 768] instead of the orignal [num_envs, 3];
+        self.language_vel_command = torch.zeros(self.num_envs, self.init_context.shape[1], device=self.device) # define a tensor, data type is str, shape is [num_envs, 768]
+        self.language_vel_command[:] = self.init_context
+        # pdj: using dragon-multiturn context encoder to encode the velocity commands
+
+        self.heading_target = torch.zeros(self.num_envs, device=self.device)
+        self.is_heading_env = torch.zeros(self.num_envs, dtype=torch.bool, device=self.device)
+        self.is_standing_env = torch.zeros_like(self.is_heading_env)
+        # -- metrics
+        self.metrics["error_vel_xy"] = torch.zeros(self.num_envs, device=self.device)
+        self.metrics["error_vel_yaw"] = torch.zeros(self.num_envs, device=self.device)
+
+    def __str__(self) -> str:
+        """Return a string representation of the command generator."""
+        msg = "LMVelocityCommandCfg:\n"
+        msg += f"\tCommand dimension: {tuple(self.command.shape[1:])}\n"
+        msg += f"\tResampling time range: {self.cfg.resampling_time_range}\n"
+        msg += f"\tHeading command: {self.cfg.heading_command}\n"
+        if self.cfg.heading_command:
+            msg += f"\tHeading probability: {self.cfg.rel_heading_envs}\n"
+        msg += f"\tStanding probability: {self.cfg.rel_standing_envs}"
+        return msg
+
+    """
+    Properties
+    """
+
+    @property
+    def command(self) -> torch.Tensor:
+        """The desired base velocity command in the base frame. Shape is (num_envs, 3)."""
+        # return self.vel_command_b
+        return self.language_vel_command
+
+    """
+    Implementation specific functions.
+    """
+
+    def _update_metrics(self):
+        # time for which the command was executed
+        max_command_time = self.cfg.resampling_time_range[1]
+        max_command_step = max_command_time / self._env.step_dt
+        # logs data
+        self.metrics["error_vel_xy"] += (
+            torch.norm(self.vel_command_b[:, :2] - self.robot.data.root_lin_vel_b[:, :2], dim=-1) / max_command_step
+        )
+        self.metrics["error_vel_yaw"] += (
+            torch.abs(self.vel_command_b[:, 2] - self.robot.data.root_ang_vel_b[:, 2]) / max_command_step
+        )
+
+    def _resample_command(self, env_ids: Sequence[int]):
+        # sample velocity commands
+        r = torch.empty(len(env_ids), device=self.device)
+        # -- linear velocity - x direction
+        self.vel_command_b[env_ids, 0] = r.uniform_(*self.cfg.ranges.lin_vel_x)
+        # -- linear velocity - y direction
+        self.vel_command_b[env_ids, 1] = r.uniform_(*self.cfg.ranges.lin_vel_y)
+        # -- ang vel yaw - rotation around z
+        self.vel_command_b[env_ids, 2] = r.uniform_(*self.cfg.ranges.ang_vel_z)
+
+        # pdj： prepare the language_vel_command. how to encode and update string in tensors?
+        for env_id in env_ids:
+            '''pdj: use the updated vel_command_b to generate the language_vel_command'''
+            # 1. prepare the expression string
+            self.env_id_express = "I am running at longitudinal speed: {} m/s, lateral speed: {} m/s, spinning speed: {} m/s.".format(self.vel_command_b[env_ids, 0], self.vel_command_b[env_ids, 1], self.vel_command_b[env_ids, 2])
+            # # 2. tokenize the string and update the tokens tensor
+            self.env_id_tokens = self.tokenizer(self.env_id_express, padding=True, truncation=True, max_length=512, return_tensors='pt')
+            # # 3. pooling the expression tokens
+            self.env_id_context = self.context_encoder(**self.env_id_tokens).pooler_output # (1, emb_dim=768)
+            # # 4. update language commands tokens
+            # self.language_vel_command[env_id] = self.env_id_context
+        
+        self.language_vel_command[env_ids, :] = self.language_vel_command[env_ids, :]
+
+        # heading target
+        if self.cfg.heading_command:
+            self.heading_target[env_ids] = r.uniform_(*self.cfg.ranges.heading)
+            # update heading envs
+            self.is_heading_env[env_ids] = r.uniform_(0.0, 1.0) <= self.cfg.rel_heading_envs
+        # update standing envs
+        self.is_standing_env[env_ids] = r.uniform_(0.0, 1.0) <= self.cfg.rel_standing_envs
+
+    def _update_command(self):
+        """Post-processes the velocity command.
+
+        This function sets velocity command to zero for standing environments and computes angular
+        velocity from heading direction if the heading_command flag is set.
+        """
+        # Compute angular velocity from heading direction
+        if self.cfg.heading_command:
+            # resolve indices of heading envs
+            env_ids = self.is_heading_env.nonzero(as_tuple=False).flatten()
+            # compute angular velocity
+            heading_error = math_utils.wrap_to_pi(self.heading_target[env_ids] - self.robot.data.heading_w[env_ids])
+            self.vel_command_b[env_ids, 2] = torch.clip(
+                self.cfg.heading_control_stiffness * heading_error,
+                min=self.cfg.ranges.ang_vel_z[0],
+                max=self.cfg.ranges.ang_vel_z[1],
+            )
+        # Enforce standing (i.e., zero velocity command) for standing envs
+        # TODO: check if conversion is needed
+        standing_env_ids = self.is_standing_env.nonzero(as_tuple=False).flatten()
+        self.vel_command_b[standing_env_ids, :] = 0.0
+
+    def _set_debug_vis_impl(self, debug_vis: bool):
+        # set visibility of markers
+        # note: parent only deals with callbacks. not their visibility
+        if debug_vis:
+            # create markers if necessary for the first tome
+            if not hasattr(self, "goal_vel_visualizer"):
+                # -- goal
+                self.goal_vel_visualizer = VisualizationMarkers(self.cfg.goal_vel_visualizer_cfg)
+                # -- current
+                self.current_vel_visualizer = VisualizationMarkers(self.cfg.current_vel_visualizer_cfg)
+            # set their visibility to true
+            self.goal_vel_visualizer.set_visibility(True)
+            self.current_vel_visualizer.set_visibility(True)
+        else:
+            if hasattr(self, "goal_vel_visualizer"):
+                self.goal_vel_visualizer.set_visibility(False)
+                self.current_vel_visualizer.set_visibility(False)
+
+    def _debug_vis_callback(self, event):
+        # check if robot is initialized
+        # note: this is needed in-case the robot is de-initialized. we can't access the data
+        if not self.robot.is_initialized:
+            return
+        # get marker location
+        # -- base state
+        base_pos_w = self.robot.data.root_pos_w.clone()
+        base_pos_w[:, 2] += 0.5
+        # -- resolve the scales and quaternions
+        vel_des_arrow_scale, vel_des_arrow_quat = self._resolve_xy_velocity_to_arrow(self.command[:, :2])
+        vel_arrow_scale, vel_arrow_quat = self._resolve_xy_velocity_to_arrow(self.robot.data.root_lin_vel_b[:, :2])
+        # display markers
+        self.goal_vel_visualizer.visualize(base_pos_w, vel_des_arrow_quat, vel_des_arrow_scale)
+        self.current_vel_visualizer.visualize(base_pos_w, vel_arrow_quat, vel_arrow_scale)
+
+    """
+    Internal helpers.
+    """
+
+    def _resolve_xy_velocity_to_arrow(self, xy_velocity: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        """Converts the XY base velocity command to arrow direction rotation."""
+        # obtain default scale of the marker
+        default_scale = self.goal_vel_visualizer.cfg.markers["arrow"].scale
+        # arrow-scale
+        arrow_scale = torch.tensor(default_scale, device=self.device).repeat(xy_velocity.shape[0], 1)
+        arrow_scale[:, 0] *= torch.linalg.norm(xy_velocity, dim=1) * 3.0
+        # arrow-direction
+        heading_angle = torch.atan2(xy_velocity[:, 1], xy_velocity[:, 0])
+        zeros = torch.zeros_like(heading_angle)
+        arrow_quat = math_utils.quat_from_euler_xyz(zeros, zeros, heading_angle)
+        # convert everything back from base to world frame
+        base_quat_w = self.robot.data.root_quat_w
+        arrow_quat = math_utils.quat_mul(base_quat_w, arrow_quat)
+
+        return arrow_scale, arrow_quat
+    
 
 class NormalVelocityCommand(UniformVelocityCommand):
     """Command generator that generates a velocity command in SE(2) from a normal distribution.
--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/__init__.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/manager_based/locomotion/velocity/config/go2/__init__.py
@@ -6,6 +6,7 @@
 import gymnasium as gym
 
 from . import agents, flat_env_cfg, rough_env_cfg
+from . import lm_flat_env_cfg, lm_rough_env_cfg # pdj: tasks configuration for language-motion training
 
 ##
 # Register Gym environments.
@@ -51,6 +52,54 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": rough_env_cfg.UnitreeGo2RoughEnvCfg_PLAY,
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:UnitreeGo2RoughPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
+    },
+)
+
+
+
+# pdj: register the language-motion tasks 
+
+gym.register(
+    id="Isaac-LM-Velocity-Flat-Unitree-Go2-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": lm_flat_env_cfg.UnitreeGo2FlatEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.lm_rsl_rl_ppo_cfg:UnitreeGo2FlatPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-LM-Velocity-Flat-Unitree-Go2-Play-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": lm_flat_env_cfg.UnitreeGo2FlatEnvCfg_PLAY,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.lm_rsl_rl_ppo_cfg:UnitreeGo2FlatPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_flat_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-LM-Velocity-Rough-Unitree-Go2-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": lm_rough_env_cfg.UnitreeGo2RoughEnvCfg,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.lm_rsl_rl_ppo_cfg:UnitreeGo2RoughPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
+    },
+)
+
+gym.register(
+    id="Isaac-LM-Velocity-Rough-Unitree-Go2-Play-v0",
+    entry_point="omni.isaac.lab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": lm_rough_env_cfg.UnitreeGo2RoughEnvCfg_PLAY,
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.lm_rsl_rl_ppo_cfg:UnitreeGo2RoughPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
     },
 )
